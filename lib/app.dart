e5648281987e0dc1958b32c33216c35076726e11
woods-one <0820ricer@gmail.com>
2024-12-24 13:02:37 +0900

    追加 : 画面遷移の機能を追加

--- lib/app.dart
+++ lib/app.dart
@@ -19,7 +19,7 @@ class App extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Hanazome',
       ),
-      home: const HomePage(title: 'GoMeReal.'),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'top_page.dart';
 import 'photo_page.dart';
 import 'map_page.dart';
 
 final _indexProvider = StateProvider((ref) => 0);
-//final _pageList = [ChatPage(), AccountPage()];
+final _pageList = [TopPage(), PhotoPage(), MapPage()];
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
-
+class HomePage extends ConsumerWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pageIndex = ref.watch(_indexProvider);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(widget.title),
         leading:
             IconButton(onPressed: () => {}, icon: const Icon(Icons.people)),
         actions: [
@@ -32,54 +24,26 @@ class _HomePageState extends State<HomePage> {
         shape: const Border(
             bottom: BorderSide(color: Color(0xff818181), width: 1)),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 64,
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.account_circle),
-                  Text("umiteru2004")
-                ],
-              ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset("assets/images/novaskin-wallpaper-ocean.jpeg"),
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Icon(Icons.favorite_border),
-            )
-          ],
-        ),
+      body: IndexedStack(
+        index: pageIndex,
+        children: _pageList,
       ),
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-            border:
-                Border(top: BorderSide(color: Color(0xff818181), width: 1))),
-        child: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.image),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.location_pin),
-              label: "",
-            ),
-          ],
-          selectedItemColor: Colors.black,
-          backgroundColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "ホーム"),
+          BottomNavigationBarItem(icon: Icon(Icons.image), label: "写真"),
+          BottomNavigationBarItem(icon: Icon(Icons.location_pin), label: "マップ")
+        ],
+        selectedItemColor: Colors.black,
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: pageIndex,
+        onTap: (index) {
+          if (pageIndex != index) {
+            ref.read(_indexProvider.notifier).state = index;
+          }
+        },
       ),
       backgroundColor: Colors.white,
     );
