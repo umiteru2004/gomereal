2a44a4a727b96b89988ac12cbbfa710332f64563
woods-one <0820ricer@gmail.com>
2024-12-24 12:24:07 +0900

    変更 : メインで処理せずページに区切ってやったりした

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
+
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
   );
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GoMeReal.',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        fontFamily: 'Hanazome',
-      ),
-      home: const MyHomePage(title: 'GoMeReal.'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(widget.title),
-        leading:
-            IconButton(onPressed: () => {}, icon: const Icon(Icons.people)),
-        actions: [
-          IconButton(
-              onPressed: () => {}, icon: const Icon(Icons.account_circle))
-        ],
-        shape: const Border(
-            bottom: BorderSide(color: Color(0xff818181), width: 1)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 64,
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.account_circle),
-                  Text("umiteru2004")
-                ],
-              ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset("assets/images/novaskin-wallpaper-ocean.jpeg"),
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Icon(Icons.favorite_border),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-            border:
-                Border(top: BorderSide(color: Color(0xff818181), width: 1))),
-        child: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.image),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.location_pin),
-              label: "",
-            ),
-          ],
-          selectedItemColor: Colors.black,
-          backgroundColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-        ),
-      ),
-      backgroundColor: Colors.white,
-    );
-  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'photo_page.dart';
+import 'map_page.dart';
+
+final _indexProvider = StateProvider((ref) => 0);
+//final _pageList = [ChatPage(), AccountPage()];
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(widget.title),
+        leading:
+            IconButton(onPressed: () => {}, icon: const Icon(Icons.people)),
+        actions: [
+          IconButton(
+              onPressed: () => {}, icon: const Icon(Icons.account_circle))
+        ],
+        shape: const Border(
+            bottom: BorderSide(color: Color(0xff818181), width: 1)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(
+              height: 64,
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.account_circle),
+                  Text("umiteru2004")
+                ],
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset("assets/images/novaskin-wallpaper-ocean.jpeg"),
+            ),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.favorite_border),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+            border:
+                Border(top: BorderSide(color: Color(0xff818181), width: 1))),
+        child: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.image),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_pin),
+              label: "",
+            ),
+          ],
+          selectedItemColor: Colors.black,
+          backgroundColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
